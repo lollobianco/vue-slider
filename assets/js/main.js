28fafecf723476b3a1467cde6a52902f6f1d6da8
lollobianco <loreb2002@gmail.com>
2022-11-11 13:54:14 +0100

    Creata struttura di partenza

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+   el: '#app',
+   data: {
+     
+      
+   },
+   methods: {
+     
+
+   }
+ })
+ 
+ // - realizzare uno slider
+       // - ci sono anche 2 frecce a sinistra e a destra dell'immagine momentanea dello slider che permettono di andare avanti e indietro tra le immagini
+ 
+       //BONUS:
+       // - nello slider ci sono dei pallini in basso al click dei pallini cambia l'immagine nello slider
+       // - a ogni 3 sec l'immagine cambia automaticamente senza nessun click da parte nostra
+ 
