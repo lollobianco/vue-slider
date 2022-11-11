1c6fb7818085561654c8b01f4af8919401894366
lollobianco <loreb2002@gmail.com>
2022-11-11 15:08:22 +0100

    Imporate foto e iniziato esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,26 @@
-var app = new Vue({
-   el: '#app',
-   data: {
-     
-      
-   },
-   methods: {
-     
+var app = new Vue(
+	{
+	   el: '#app',
+	      data:{
 
-   }
- })
+            images: [ './assets/img/1-990-600x300.jpg', './assets/img/193-600x300.jpg', './assets/img/403-600x300.jpg', './assets/img/517-600x300.jpg', './assets/img/8-600x300.jpg', './assets/img/880-600x300.jpg',],
+
+
+	      },
+		   methods:{
+			
+            changePhotoUP: function (){
+
+               this.index++;
+               if( this.index > this.images.length ){
+                 return this.index = 0;
+               } 
+
+            }
+		
+		   }
+	}
+);
  
  // - realizzare uno slider
        // - ci sono anche 2 frecce a sinistra e a destra dell'immagine momentanea dello slider che permettono di andare avanti e indietro tra le immagini
